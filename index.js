f0be3e6da9b9faef62368373ada606737dc1f5b9
Grant Richmond <mail@grant.codes>
2019-07-29 01:05:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const emojiList = require('emojis-list');
+
+/**
+ * Checks if a string is a single emoji
+ * @param {string} text String to test
+ * @returns {boolean} True if text is a single emoji
+ */
+const isSingleEmoji = text => emojiList.indexOf(text.trim()) > -1;
+
+// TODO: Certain properties maybe should be checked to see if they are a url or a nested mf2 object
+/**
+ * Accepts a microformats 2 object and attempts to determine the post type
+ * @param {object} mf2 A valid microformats 2 object
+ * @returns {string|null} The post type or null if unknown
+ */
+function ptd(mf2) {
+  // If is not h-entry then use that for the post type. E.g. event, recipe, resume etc.
+  if (mf2.type && mf2.type[0] !== 'h-entry' && mf2.type[0].startsWith('h-')) {
+    return mf2.type[0].substring(2);
+  }
+
+  /**
+   * Checks that the microformats object contains the givin property and it is not empty
+   * @param {string} property Property name to check for
+   * @returns {boolean} True if exists and not empty
+   */
+  const propertyExists = property =>
+    mf2.properties &&
+    mf2.properties[property] &&
+    Array.isArray(mf2.properties[property]) &&
+    typeof mf2.properties[property][0] !== 'undefined' &&
+    mf2.properties[property][0] !== null &&
+    mf2.properties[property][0].trim() !== '';
+
+  // Get the main content of the post
+  let content = null;
+  if (propertyExists('content')) {
+    [content] = mf2.properties.content;
+  } else if (propertyExists('summary')) {
+    [content] = mf2.properties.summary;
+  }
+  if (content && typeof content !== 'string') {
+    if (content.value) {
+      content = content.value;
+    } else if (content.html) {
+      content = content.html;
+    }
+  }
+  if (content) {
+    content = content.trim();
+  }
+
+  // Then continue to base post type discovery
+  if (propertyExists('rsvp')) {
+    return 'rsvp';
+  }
+  if (propertyExists('in-reply-to')) {
+    if (content && isSingleEmoji(content)) {
+      return 'reacji';
+    }
+    return 'reply';
+  }
+  if (propertyExists('repost-of')) {
+    return 'repost';
+  }
+  if (propertyExists('bookmark-of')) {
+    return 'bookmark';
+  }
+  if (propertyExists('quotation-of')) {
+    return 'quotation';
+  }
+  if (propertyExists('like-of')) {
+    return 'like';
+  }
+  if (propertyExists('checkin')) {
+    return 'checkin';
+  }
+  if (propertyExists('listen-of')) {
+    return 'listen';
+  }
+  if (propertyExists('read-of')) {
+    return 'read';
+  }
+  if (propertyExists('watch-of')) {
+    return 'watch';
+  }
+  if (propertyExists('isbn')) {
+    return 'book';
+  }
+  if (propertyExists('video')) {
+    return 'video';
+  }
+  if (propertyExists('audio')) {
+    return 'audio';
+  }
+  if (propertyExists('ate')) {
+    return 'ate';
+  }
+  if (propertyExists('drank')) {
+    return 'drank';
+  }
+  if (mf2.children && Array.isArray(mf2.children) && mf2.children.length > 0) {
+    return 'collection';
+  }
+  if (propertyExists('photo')) {
+    return 'photo';
+  }
+  if (propertyExists('name') && propertyExists('content')) {
+    const name = mf2.properties.name[0].trim();
+    if (!content.startsWith(name)) {
+      return 'article';
+    }
+  }
+
+  return 'note';
+}
+
+module.exports = ptd;
---
+++ tests/index.test.js
@@ -0,0 +1,255 @@
+const ptd = require('../index');
+
+test('basic note post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      content: ['Basic note'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('note');
+});
+
+test('note with name post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      name: ['Title'],
+      content: ['Title: Note content'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('note');
+});
+
+test('article post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      name: ['Title'],
+      content: ['content'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('article');
+});
+
+test('photo post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      photo: ['https://example.com/photo.jpg'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('photo');
+});
+
+test('video post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      video: ['https://example.com/video.mp4'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('video');
+});
+
+test('audio post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      audio: ['https://example.com/audio.mp3'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('audio');
+});
+
+test('like post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'like-of': ['https://example.com'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('like');
+});
+
+test('repost post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'repost-of': ['https://example.com'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('repost');
+});
+
+test('bookmark post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'bookmark-of': ['https://example.com'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('bookmark');
+});
+
+test('quotation post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'quotation-of': ['https://example.com'],
+      content: ['This is a quote'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('quotation');
+});
+
+test('rsvp post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      rsvp: ['yes'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('rsvp');
+});
+
+test('reply post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'in-reply-to': ['https://example.com'],
+      content: ['Reply text'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('reply');
+});
+
+test('reactji post type discovery', () => {
+  const mf2 = emoji => ({
+    type: ['h-entry'],
+    properties: {
+      'in-reply-to': ['https://example.com'],
+      content: [emoji],
+    },
+  });
+  const type = ptd(mf2('👩'));
+  expect(type).toBe('reacji');
+  const type2 = ptd(mf2('👨‍👨‍👧‍👧'));
+  expect(type2).toBe('reacji');
+  // TODO: Complex emoji maybe doesn't work
+  // const type3 = ptd(mf2("👩🏻‍🤝‍🧑🏿"));
+  // expect(type3).toBe("reacji");
+});
+
+test('watch post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'watch-of': ['https://example.com/video.mp4'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('watch');
+});
+
+test('listen post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'listen-of': ['https://example.com/audio.mp3'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('listen');
+});
+
+test('read post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      'read-of': ['https://example.com/article'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('read');
+});
+
+test('checkin post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      checkin: ['https://example.com/place'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('checkin');
+});
+
+test('collection post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      name: ['Collection'],
+    },
+    children: ['https://example.com/child-1', 'https://example.com/child-2'],
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('collection');
+});
+
+test('event post type discovery', () => {
+  const mf2 = {
+    type: ['h-event'],
+    properties: {
+      name: ['Event title'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('event');
+});
+
+test('book post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      name: ['Book title'],
+      isbn: ['isbnnumber'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('book');
+});
+
+test('ate post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      ate: ['Chicken Salad'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('ate');
+});
+
+test('drank post type discovery', () => {
+  const mf2 = {
+    type: ['h-entry'],
+    properties: {
+      drank: ['water'],
+    },
+  };
+  const type = ptd(mf2);
+  expect(type).toBe('drank');
+});
